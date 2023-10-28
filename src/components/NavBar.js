df7e0b2357bb4c3f40f2fb1494a0970cfc23d29a
Alex Rofes <alexrofes1992@gmail.com>
2023-10-28 19:39:35 +0200

    botones

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,10 @@ import icon2 from '../assets/img/icon2.png';
 import icon3 from '../assets/img/icon3.png';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from '../assets/img/logo.png';
+import { HashLink } from 'react-router-hash-link';
+import {
+    BrowserRouter as Router
+} from "react-router-dom";
 
 
 export function NavBar() {
@@ -30,35 +34,35 @@ export function NavBar() {
     }
 
     return (
-        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} alt='Logo' />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-                    </Nav>
-                    <span className="navbar-text">
-                        <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/s%C3%A4deleer%C3%A4st/"><img src={icon1} alt="" /></a>
-                            <a href="https://www.facebook.com/sadelee.rast?locale=es_ES"><img src={icon2} alt="" /></a>
-                            <a href="https://www.instagram.com/sadelee.rast/"><img src={icon3} alt="" /></a>
-                        </div>
-                        <button className="btn btn-primary" onClick={() => console.log('connect')}>
-                            <span>
-                                Let's Connect
-                            </span>
-                        </button>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Router>
+            <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src={logo} alt='Logo' />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav">
+                        <span className="navbar-toggler-icon"></span>
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                        </Nav>
+                        <span className="navbar-text">
+                            <div className="social-icon">
+                                <a href="https://www.linkedin.com/in/s%C3%A4deleer%C3%A4st/"><img src={icon1} alt="" /></a>
+                                <a href="https://www.facebook.com/sadelee.rast?locale=es_ES"><img src={icon2} alt="" /></a>
+                                <a href="https://www.instagram.com/sadelee.rast/"><img src={icon3} alt="" /></a>
+                            </div>
+                            <HashLink to='#connect'>
+                                <button className="navbar-text button"><span>Let’s Connect</span></button>
+                            </HashLink>
+                        </span>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Router>
     );
 }
 
