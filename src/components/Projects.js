e8dffe30dd482ff644b8e2d10268c1962f7e4997
Alex Rofes <alexrofes1992@gmail.com>
2023-11-03 20:13:07 +0100

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import { ProjectCard } from "./ProjectCard";
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+
 
 
 
@@ -26,71 +26,51 @@ export const Projects = () => {
             description: "Design & Development",
             imgUrl: projImg3,
         },
-        // {
-        //     title: "Business Startup",
-        //     description: "Design & Development",
-        //     imgUrl: projImg1,
-        // },
-        // {
-        //     title: "Business Startup",
-        //     description: "Design & Development",
-        //     imgUrl: projImg2,
-        // },
-        // {
-        //     title: "Business Startup",
-        //     description: "Design & Development",
-        //     imgUrl: projImg3,
-        // },
-
+        // Más objetos de proyectos aquí...
     ];
-    return (
 
+    return (
         <section className="project" id="projects">
             <Container>
                 <Row>
                     <Col className="project-col" size={12}>
-                        <TrackVisibility>
-                            {({ isVisible }) =>
-                                <div className={isVisible ? "animate__animated animate__bounce" : ""}>
-                                    <h2>Projects</h2>
-                                    <p>Explore this compilation of works that vividly reflect the passion I have for content creation. Each piece is a testament to my commitment to engaging storytelling and creative expression.</p>
-                                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                                        <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="first">Travel</Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="second">Food</Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="third">Other</Nav.Link>
-                                            </Nav.Item>
-                                        </Nav>
-                                        <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                                            <Tab.Pane eventKey="first">
-                                                <Row>
-                                                    {
-                                                        projects.map((project, index) => {
-                                                            return (
-                                                                <ProjectCard
-                                                                    key={index}
-                                                                    {...project}
-                                                                />
-                                                            )
-                                                        })
-                                                    }
-                                                </Row>
-                                            </Tab.Pane>
-                                            <Tab.Pane eventKey="section">
-                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                                            </Tab.Pane>
-                                            <Tab.Pane eventKey="third">
-                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                                            </Tab.Pane>
-                                        </Tab.Content>
-                                    </Tab.Container>
-                                </div>}
-                        </TrackVisibility>
+                        <h2>Projects</h2>
+                        <p>Explore this compilation of works that vividly reflect the passion I have for content creation. Each piece is a testament to my commitment to engaging storytelling and creative expression.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Travel</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Food</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Other</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content id="slideInUp">
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <ProjectCard
+                                                        key={index}
+                                                        {...project}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="section">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
