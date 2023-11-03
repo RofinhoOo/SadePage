10a13a9437765d79ab40fb83a5a81d2a6f7490fe
Alex Rofes <alexrofes1992@gmail.com>
2023-11-03 21:10:56 +0100

    refresh

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,89 +13,79 @@ export const Contact = () => {
       <Container>
         <Row className="item-form">
           <Col size={12} md={6}>
-              <TrackVisibility offset={-100}>
-                {({ isVisible }) => (
-                  <img
-                    id="contact-image"
-                    src={contactImg}
-                    alt="Contact Us"
-                    className={isVisible ? "animate__animated animate__zoomIn" : ""}
-                  />
-                )}
-              </TrackVisibility>
-          </Col>
-          <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) => (
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <h2>Get In Touch</h2>
-                  <form onSubmit={async (e) => {
-                    e.preventDefault(); // Previene el comportamiento predeterminado del formulario
-                    await handleSubmit(e); // Envía el formulario
-                    // Agrega cualquier lógica adicional que desees ejecutar después del envío del formulario.
-                  }}>
-                    <Row>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="text"
-                          id="firstName"
-                          name="firstName"
-                          placeholder="First Name"
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="text"
-                          id="lastName"
-                          name="lastName"
-                          placeholder="Last Name"
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="email"
-                          id="email"
-                          name="email"
-                          placeholder="Email Address"
-                        />
-                        <ValidationError
-                          prefix="Email"
-                          field="email"
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="tel"
-                          id="phone"
-                          name="phone"
-                          placeholder="Phone Number"
-                        />
-                      </Col>
-                      <Col size={12} className="px-1">
-                        <textarea
-                          rows="6"
-                          id="message"
-                          name="message"
-                          placeholder="Your Message"
-                        />
-                        <ValidationError
-                          prefix="Message"
-                          field="message"
-                        />
-                        <button type="submit" disabled={state.submitting}>
-                          <span>{state.submitting ? "Sending..." : "Send"}</span>
-                        </button>
-                      </Col>
-                      {state.succeeded && (
-                        <Col>
-                          <p className="success">Message sent successfully</p>
-                        </Col>
-                      )}
-                    </Row>
-                  </form>
-                </div>
+                <img
+                  id="contact-image"
+                  src={contactImg}
+                  alt="Contact Us"
+                  className={isVisible ? "animate__animated animate__zoomIn" : ""}
+                />
               )}
             </TrackVisibility>
+          </Col>
+          <Col size={12} md={6}>
+            <h2>Get In Touch</h2>
+            <form onSubmit={handleSubmit}>
+              <Row>
+                <Col size={12} sm={6} className="px-1">
+                  <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    placeholder="First Name"
+                  />
+                </Col>
+                <Col size={12} sm={6} className="px-1">
+                  <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    placeholder="Last Name"
+                  />
+                </Col>
+                <Col size={12} sm={6} className="px-1">
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email Address"
+                  />
+                  <ValidationError
+                    prefix="Email"
+                    field="email"
+                  />
+                </Col>
+                <Col size={12} sm={6} className="px-1">
+                  <input
+                    type="tel"
+                    id="phone"
+                    name="phone"
+                    placeholder="Phone Number"
+                  />
+                </Col>
+                <Col size={12} className="px-1">
+                  <textarea
+                    rows="6"
+                    id="message"
+                    name="message"
+                    placeholder="Your Message"
+                  />
+                  <ValidationError
+                    prefix="Message"
+                    field="message"
+                  />
+                  <button type="submit" disabled={state.submitting}>
+                    <span>{state.submitting ? "Sending..." : "Send"}</span>
+                  </button>
+                </Col>
+                {state.succeeded && (
+                  <Col>
+                    <p className="success">Message sent successfully</p>
+                  </Col>
+                )}
+              </Row>
+            </form>
           </Col>
         </Row>
       </Container>
