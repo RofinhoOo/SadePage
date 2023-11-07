33f0aace781d48a07b12fd9f2db9a6cbc3416886
Alex Rofes <alexrofes1992@gmail.com>
2023-11-07 20:43:23 +0100

    video

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,7 +7,7 @@ export const ProjectCard = ({ title, description, videoUrl }) => {
             <div className="project-card-inner">
                 {videoUrl ? (
                     <div className="video-container">
-                        <video className="video" controls>
+                        <video className="video" controls autoplay>
                             <source src={videoUrl} type="video/mp4" />
                             Tu navegador no soporta la reproducción de video.
                         </video>
