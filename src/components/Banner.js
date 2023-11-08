546c2b61d1a4df9a11c4cb300d6dce64e7f1502a
Alex Rofes <alexrofes1992@gmail.com>
2023-11-08 19:49:32 +0100

    fin

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,7 +13,7 @@ export const Banner = () => {
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const [index, setIndex] = useState(1);
-    const toRotate = ["Community Manager", "Content Creator", "Instagramer"];
+    const toRotate = ["Community Manager", "Content Creator"];
     const period = 2000;
 
     useEffect(() => {
